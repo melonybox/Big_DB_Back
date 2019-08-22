01b3354b1fc4d8722629fd35f87fc43dda3611ab
melonybox <grandmadcow@gmail.com>
2019-08-22 18:27:03 -0400

    fixed users controller to work with api v1 path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
-    render json: @users 
+    render json: @users
   end
 end
