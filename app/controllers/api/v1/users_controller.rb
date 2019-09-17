931660980072f90a88f90c41f9143d222bc61f60
melonybox <grandmadcow@gmail.com>
2019-09-17 11:07:49 -0400

    Cache on in dev, cache working with jayson api.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.getAll
     render json: @users
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def self.getAll
+    Rails.cache.fetch("getAll", expires_in: 1.hour){ User.all.to_json }
+  end
 end
