def3cb8e1b126848901842f8eb7beaaf6c8a28fc
melonybox <grandmadcow@gmail.com>
2019-10-01 11:33:42 -0400

    Add custom route for amount fetch, added custom function in users controller for grabbing limit amount

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::UsersController < ApplicationController
     @users = User.getAll
     render json: @users
   end
+
+  def user_list
+    @userlist = User.limit(params[:amount])
+    render json: @userlist
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+
+      get "/userlist/:amount", to: "users#user_list"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
