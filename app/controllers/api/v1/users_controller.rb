541d71088ef32e6c4b6c5af4c1f333d29a07e3e1
melonybox <grandmadcow@gmail.com>
2019-10-19 20:18:21 -0400

    modified datasent to include currpage for paginationing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
   def user_list
     userList = User.offset(params[:page].to_i*params[:amount].to_i).limit(params[:amount].to_i)
     pageCount = User.count / params[:amount].to_i
-    dataSent = [userList,pageCount]
+    dataSent = [userList,pageCount,params[:page].to_i]
     render json: dataSent
   end
 end
