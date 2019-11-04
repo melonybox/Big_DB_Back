0be874ee266f963a01f4c427644a1e54812e207d
melonybox <grandmadcow@gmail.com>
2019-11-03 19:44:58 -0500

    JS code added to convert to Ruby

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,7 @@ class Api::V1::UsersController < ApplicationController
     userList = User.offset(params[:page].to_i*params[:amount].to_i).limit(params[:amount].to_i)
     pageCount = User.count / params[:amount].to_i
     dataSent = [userList,pageCount,params[:page].to_i]
+    debugger
     render json: dataSent
   end
 end
@@ -19,7 +20,7 @@ end
 #           return <PageComponent key={i} pageNum={i+1} viewCount={this.props.viewCount} />
 #         } else if (this.props.currPage+3 > this.props.totalPages && this.props.totalPages >= 5) {
 #           return <PageComponent key={i} pageNum={((this.props.totalPages-5)+i)+1} viewCount={this.props.viewCount} />
-#         } else if (this.props.totalPages >= 5){
+#         } else if (this.props.totalPages >= 5) {
 #           return <PageComponent key={i} pageNum={((this.props.currPage-2)+i)+1} viewCount={this.props.viewCount} />
 #         }
 #       })}
