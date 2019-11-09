d91608a4e817b27a5291bc0e4bce498c314e00e1
melonybox <grandmadcow@gmail.com>
2019-11-09 18:46:08 -0500

    Order by type and order works.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_list
-    userList = User.offset(params[:page].to_i*params[:amount].to_i).limit(params[:amount].to_i)
+    userList = User.order('id ASC').offset(params[:page].to_i*params[:amount].to_i).limit(params[:amount].to_i)
     pageCount = User.count / params[:amount].to_i
     pageList =
     if pageCount <= 5
